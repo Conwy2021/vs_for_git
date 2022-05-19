b24725bebfb948e4c7f80af04e35fb99b2d94d61
Conwy2021 <16602889867@163.com>
2022-05-19 15:15:46 +0800

    增加test3接口 重定向漏洞代码

--- src/main/java/com/example/demo/controller/testController.java
+++ src/main/java/com/example/demo/controller/testController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +21,15 @@ public class testController {
     public String test2(){
         System.out.println("test2");
         return "test2";
+    }
+
+    @RequestMapping("test3")
+    public void test3(HttpServletRequest request,HttpServletResponse response) throws IOException{
+        String url =request.getParameter("url");
+
+        System.out.println(url);
+
+        response.sendRedirect(url);
+        
     }
 }
