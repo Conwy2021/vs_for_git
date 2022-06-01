996c5197aa6c5fff121e75812b675a91d099af75
Conwy2021 <16602889867@163.com>
2022-06-01 16:56:46 +0800

    新增ssrf漏洞 md新增漏洞使用说明

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # vs_for_git
 有代码漏洞 测试代码
+项目启动 java -jar demo-0.0.1-SNAPSHOT.jar
+漏洞使用说明
+http://192.xx.xx.xx:8080/test
+http://192.xx.xx.xx:8080/test2  这两个用于测试启动正常不  有没有防火墙  白名单等
+
+http://192.xx.xx.xx:8080/test3?url=http://www.baidu.com url跳转漏洞
+
+http://192.xx.xx.xx:8080/test4?url=http://www.baidu.com ssrf调整漏洞
+ 
--- src/main/java/com/example/demo/controller/testController.java
+++ src/main/java/com/example/demo/controller/testController.java
@@ -1,9 +1,16 @@
 package com.example.demo.controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +38,25 @@ public class testController {
 
         response.sendRedirect(url);
         
+    }
+
+    @RequestMapping("test4")
+    public String test4(HttpServletRequest request,HttpServletResponse response) throws IOException{
+        String url = request.getParameter("url");
+        System.out.println(url);
+        URL u = new URL(url);
+        URLConnection urlConnection = u.openConnection();
+        urlConnection.setAllowUserInteraction(true);
+        /*未对输入参数进行验证，导致访问地址可控，出现SSRF漏洞*/
+        InputStream inputStream = urlConnection.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        // 打印从服务器读取的报文
+        String line = "";
+        while ((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+        }
+
+
+        return "SSRFtest";
     }
 }
